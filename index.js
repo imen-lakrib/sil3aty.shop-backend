030b53090404f807e56a39cbb8fbe60b4f045b76
imen-lakrib <lak12imen@gmail.com>
2023-07-27 19:36:57 +0100

    delete .env

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import path from "path";
 import dotenv from "dotenv"
 import connectDb from "./config/db.js"
+import nodemailer from "nodemailer";
 
 
 
@@ -14,6 +15,7 @@ import WishList from './routes/WishList.js'
 import CategoryRoute from './routes/Category.js'
 import ColorRoute from './routes/Color.js'
 import SizeRoute from './routes/Size.js'
+import SendEmail from "./utils/SendEmail.js";
 
 dotenv.config();
 connectDb()
@@ -54,6 +56,32 @@ app.use('/sizes',SizeRoute)
 app.use("/test",(req,res)=>{
   res.send("hello world")
 })
+
+
+app.post("/send-email", async (req, res) => {
+  try {
+    const { fullname, email, message } = req.body;
+
+    // Create the mail options
+    const mailOptions = {
+      to: "your-personal-email@example.com", // Your personal email
+      subject: "New Contact Form Submission",
+      text: `Full Name: ${fullname}\nEmail: ${email}\nMessage: ${message}`,
+    };
+
+     // Send email using the SendEmail function
+     const result = await SendEmail(mailOptions);
+
+     console.log("Email sent successfully!", result);
+ 
+     res.status(200).json({ message: "Email sent successfully!" });
+   } catch (error) {
+     console.error("Error sending email:", error.message); // Log the error message
+     res.status(500).json({ error: "An error occurred while sending the email" });
+   }
+});
+
+
 
 
 app.listen(PORT, ()=> console.log(`listening on port : ${PORT}`));
--- utils/SendEmail.js
+++ utils/SendEmail.js
@@ -1,34 +1,49 @@
 import nodemailer from "nodemailer"
-
 const SendEmail = async (options) => {
+
+
+
+
+
+
+
+  try {
     // create reusable transporter object using the default SMTP transport
     const transporter = nodemailer.createTransport({
-      host: 'smtp.office365.com',
+      host: "smtp.office365.com",
       port: 587,
       secure: false,
       auth: {
-        user: 'sil3aty@outlook.com',
-        pass: '15hhhNemi7@@@@',
+        user: process.env.SENDER_MAIL,
+        pass: process.env.MAIL_PW,
+
       },
       tls: {
-        ciphers: 'SSLv3',
+        ciphers: "SSLv3",
         rejectUnauthorized: false,
       },
     });
-  
+
+    // send mail with defined transport object
     // send mail with defined transport object
     const info = await transporter.sendMail({
-      from: 'sil3aty@outlook.com', // sender address
-      to: options.to, // list of receivers
+      from: process.env.SENDER_MAIL, // sender address
+      to: process.env.RECIPIENT_Mail, // Replace with the recipient's valid email address
       subject: options.subject, // Subject line
       text: options.text, // plain text body
     });
-  
+
     console.log('Message sent: %s', info.messageId);
-  };
-  
-  export default SendEmail;
-  
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info)); // Log the preview URL if using the test account
+
+    return info; // Return the result of sending the email
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error; // Rethrow the error to handle it in the calling function
+  }
+};
+export default SendEmail;
+
 
 
 
